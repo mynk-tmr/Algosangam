ee0fd3a64a4b340016d5c5d59ba4274e26e1f720
Mayank <mynk.secure@gmail.com>
2024-01-21 22:03:38 +0530

    add codeblock to snippetcard

--- src/components/SnippetCard.jsx
+++ src/components/SnippetCard.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { ClockIco, CodeIco, EyeIco, LikeIco } from "../shared/icons";
+import CodeBlock from "./CodeBlock";
+import snippets from "../database/snippets";
 
 const Wrapper = styled.section`
   padding: 1rem;
@@ -57,20 +59,7 @@ export default function SnipperCard() {
         <CodeIco /> Javascript
       </data>
       <figure data-code>
-        <pre>
-          <code className="language-js">
-            {`
-Bun.serve({
-  fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname === "/") return new Response("Home page!");
-    if (url.pathname === "/blog") return new Response("Blog!");
-    return new Response("404!");
-  },
-});
-            `}
-          </code>
-        </pre>
+        <CodeBlock lang={snippets[0].lang}>{snippets[0].content}</CodeBlock>
       </figure>
     </Wrapper>
   );
---
+++ src/database/snippets.js
@@ -0,0 +1,19 @@
+const snippets = [
+  {
+    title: `Simple Bunjs Server`,
+    likes: `3`,
+    views: `122`,
+    createdAt: new Date(2024, 1, 3),
+    lang: `javascript`,
+    content: `Bun.serve({
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/blog") return new Response("Blog!");
+    return new Response("404!");
+  },
+});`,
+  },
+];
+
+export default snippets;
