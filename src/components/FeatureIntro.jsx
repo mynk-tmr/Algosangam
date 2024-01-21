00d765542ed80b6ee3b5fdeced166506c72a58e0
Mayank <mynk.secure@gmail.com>
2024-01-21 05:58:46 +0530

    extract feature info to database and added slider to feature component

--- src/components/FeatureIntro.jsx
+++ src/components/FeatureIntro.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import GradientText from "./GradientText";
 import { useState, Fragment } from "react";
+import { features } from "../database/data";
 
 const Section = styled.section`
   display: grid;
@@ -21,31 +22,32 @@ const Section = styled.section`
 
 export default function FeatureIntro() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const features = [
-    {
-      head: "Collections",
-      text: "Easily upload and keep track of code snippets that are important to you. Create a post, upload it, and never waste your time finding code snippets again.",
-      headcolors: ["firebrick", "orange"],
-      textcolors: ["white", "lightgreen"],
-    },
-  ];
   return (
     <Section>
       <section className="gifbox">Gif</section>
       <section className="info">
         {features.map(
-          ({ headcolors, head, textcolors, text }, index) =>
+          ({ headcolors, head, text }, index) =>
             index === activeIndex && (
               <Fragment key={index}>
                 <GradientText as="h2" size="2.5rem" $colors={headcolors}>
                   {head}
                 </GradientText>
-                <GradientText as="p" size="1.25rem" $colors={textcolors}>
+                <GradientText
+                  as="p"
+                  size="1.25rem"
+                  $colors={["white", headcolors.at(1)]}>
                   {text}
                 </GradientText>
               </Fragment>
             )
         )}
+      </section>
+      <section className="slider">
+        <button
+          onClick={() => setActiveIndex((1 + activeIndex) % features.length)}>
+          Next
+        </button>
       </section>
     </Section>
   );
---
+++ src/database/data.js
@@ -0,0 +1,22 @@
+export const features = [
+  {
+    head: "Collections",
+    text: `Easily upload and keep track of code snippets that are important to you. Add Notes with media support and never waste your time finding code snippets again.`,
+    headcolors: ["firebrick", "orange"],
+  },
+  {
+    head: "Fork and Enhance",
+    text: `Want to extend or simplify a code snippet? Fork and Edit while viewing a post and it automatically gets added to your collections`,
+    headcolors: ["#4DA0B0", "#D39D38"],
+  },
+  {
+    head: "Spaced Repetition",
+    text: `Mark snippets you want to remember and the app will take care of rest. Only recall and write code you are about to forget. Never waste time practicising things that have become easy for you.`,
+    headcolors: ["#00C9FF", "#92FE9D"],
+  },
+  {
+    head: "Codeblocks",
+    text: `Supports code compiling for 15+ languages such as JavaScript, Python, C, Go, and more. Simply click the "Run" button when viewing a post to see your output.`,
+    headcolors: ["#2C3E50", "#FD746C"],
+  },
+];
