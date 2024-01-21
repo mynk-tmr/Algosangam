1fd0972304bdc1629dde0c9eb0ff1c26e8d2c00b
Mayank <mynk.secure@gmail.com>
2024-01-21 17:32:57 +0530

    add fade-in animation to feature info text

--- src/components/FeatureIntro.jsx
+++ src/components/FeatureIntro.jsx
@@ -1,24 +1,37 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import GradientText from "./GradientText";
 import { useState } from "react";
 import { features } from "../database/data";
 import PreviewCard from "./PreviewCard";
 
+const slideIn = keyframes`
+  from {
+    translate: 0 50px;
+    opacity: 0;
+  }
+  to {
+    translate: 0;
+    opacity: 1;
+  }
+`;
+
 const Section = styled.section`
   display: grid;
   grid-template: 1fr 1fr / 1fr 1fr;
   min-height: 100vh;
+  > * {
+    padding: 3rem;
+    display: grid;
+  }
+
+  .animate {
+    animation: ${slideIn} 300ms ease-in;
+  }
 
   .gifbox {
     background: gray;
     grid-row: span 2;
     place-self: center;
-  }
-
-  .info,
-  .slider {
-    padding: 3rem;
-    display: grid;
   }
 
   .slider {
@@ -30,11 +43,16 @@ const Section = styled.section`
 export default function FeatureIntro() {
   const [activeIndex, setActiveIndex] = useState(0);
   const { head, text, colors, gif } = features[activeIndex];
-
+  const ref = (node) => {
+    if (node) {
+      node.classList.add("animate");
+      setTimeout(() => node.classList.remove("animate"), 300);
+    }
+  };
   return (
     <Section>
       <img src={gif} className="gifbox" />
-      <section className="info">
+      <section ref={ref}>
         <GradientText as="h2" size="2.5rem" $colors={colors}>
           {head}
         </GradientText>
